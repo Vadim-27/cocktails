2cfd9958e6806c00760838db3c85ef94a28ba52e
Vadim-27 <vadim.bulanyi@gmail.com>
2024-02-13 18:15:50 +0100

    feat create home page

---
+++ src/components/CocktailThumb.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+    cocktail: {
+        type: Object,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <div class="root">
+        <RouterLink :to="`/cocktail/${cocktail.idDrink}`">
+        <div :style="`background-image: url(${cocktail.strDrinkThumb})`" class="pic"></div>
+        <p class="name">{{ cocktail.strDrink }}</p>
+        </RouterLink>
+    </div>
+  
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+.root
+    width: 150px
+.pic
+    width: 150px
+    height: 150px
+    background-repeat: no-repeat
+    background-position: 50% 50%
+    margin: 0 auto
+    background-size: contain
+
+.name
+    margin-top: 15px
+    letter-spacing: 0.1em
+      
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { ref } from "vue";
+import AppLayout from "@/components/AppLayout.vue";
+import CocktailThumb from "../components/CocktailThumb.vue";
+import imgCocktail from "@/assets/img/cocktail.jpg";
+import { useRootStore } from "../stores/root";
+import { storeToRefs } from "pinia";
+
+const rootStore = useRootStore();
+rootStore.getIngredients();
+
+const { ingredients, cocktails } = storeToRefs(rootStore);
+const ingredient = ref(null);
+
+const getCocKtails = () => {
+  rootStore.getCocktails(ingredient.value);
+};
+</script>
+
+<template>
+  <AppLayout :imgUrl="imgCocktail">
+    <div class="wrapper"></div>
+    <section v-if="!ingredient || !cocktails" class="info">
+      <h1 class="title">Choose your drink</h1>
+      <div class="line"></div>
+      <div class="select-wrapper">
+        <el-select
+          v-model="ingredient"
+          class="select"
+          placeholder="Choose main ingredient"
+          size="large"
+          style="width: 240px"
+          @change="getCocKtails"
+        >
+          <el-option
+            v-for="item in ingredients"
+            :key="item.strIngredient1"
+            :label="item.strIngredient1"
+            :value="item.strIngredient1"
+          />
+        </el-select>
+      </div>
+      <p class="text">
+        Try our delicious cocktail recipes for every occasion. Find delicious
+        cocktail recipes by ingredient through our cocktail generator.
+      </p>
+      <div class="img">
+        <img
+          src="../assets/img/1665865889_66-podacha-blud-com-p-shot-kokteil-krasivie-foto-71 2.png"
+          alt="cocktails"
+        />
+      </div>
+    </section>
+    <section v-else class="info">
+      <h1 class="title">COCKTAILS WITH {{ ingredient }}</h1>
+      <div class="line"></div>
+      <div class="cocktails">
+        <CocktailThumb
+          v-for="cocktail in cocktails"
+          :key="cocktail.idDrink"
+          :cocktail="cocktail"
+        />
+      </div>
+    </section>
+  </AppLayout>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+
+.wrapper
+    display: flex
+    justify-content: center
+    align-items: center
+
+.info
+    padding: 80px
+    text-align: center
+
+.select-wrapper
+  padding-top: 50px
+
+.select
+  width: 220px
+
+.text
+  min-width: 516px
+  margin: 0 auto
+  padding-top: 50px
+  line-height: 36px
+  letter-spacing: 0.1em
+  color: $textMuted
+
+.img
+  margin-top: 60px
+
+.cocktails
+  display: flex
+  flex-wrap: wrap
+  gap: 30px
+  margin-top: 50px
+  max-height: 600px
+  overflow-y: auto
+
+</style>
